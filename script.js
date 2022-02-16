e2909928020446fb5c2e1f13fa6e01f83e7405f3
muna-an <muna.anw@gmail.com>
2022-02-16 14:18:39 +0300

    msg add

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function bookmark(){
     document.getElementById("bookmark").innerHTML = 'Bookmarked';
+    document.getElementsByClassName("two-btn-2").style.backgroundColor='hsl(176, 72%, 28%)';
+    document.getElementById("bookmark").style.color='#fff';
 }
